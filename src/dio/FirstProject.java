784c289f22614d23e6f0602f7300d82d0e483353
monicashdias <msumiedias@gmail.com>
2022-07-14 18:00:36 -0300

    first commit

---
+++ src/dio/FirstProject.java
@@ -0,0 +1,26 @@
+package dio;
+
+import dio.model.Cat;
+
+public class FirstProject {
+
+	public static void main(String[] args) {
+		
+		Cat cat = new Cat();
+		Books books = new Books();
+		
+		System.out.println(cat);
+		System.out.println(books);
+		
+	}
+		/*int a = 2;
+		int b = 3;
+		System.out.println("Hello World! " + (a+b));
+	}*/
+}
+
+
+class Books {
+	private String name;
+	private String pages;
+}
